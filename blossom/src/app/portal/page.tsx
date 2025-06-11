23dbbfbb11f5dcee4142f3716d3efc9beaf7e7fb
absceptual <absceptual@gmail.com>
2025-06-10 23:46:44 -0500

    fix: production ready

--- blossom/src/app/portal/page.tsx
+++ blossom/src/app/portal/page.tsx
@@ -3,18 +3,24 @@
 import { LoginForm, RegisterForm } from "@/components/login-form"
 
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-
-export default function Page() {
-
+function PortalContent() {
     const searchParams = useSearchParams();
     const isLogin = searchParams.get('register') !== 'true';
 
+    return isLogin ? <LoginForm /> : <RegisterForm />;
+}
+
+
+export default function Page() {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-        <div className="w-full max-w-sm">
-            {isLogin ? <LoginForm /> : <RegisterForm />}
-        </div>
+            <div className="w-full max-w-sm">    
+                <Suspense fallback={<div>Loading...</div>}> {/* Or any other loading UI */}
+                    <PortalContent />
+                </Suspense>
+            </div>
         </div>
     )
 }
