133d044bc5c2a95df6e6d8741497ebb3145914cb
absceptual <absceptual@gmail.com>
2026-03-29 01:20:54 -0500

    fix: combobox can now scroll in a popover

--- blossom/src/components/ui/combobox.tsx
+++ blossom/src/components/ui/combobox.tsx
@@ -24,7 +24,7 @@ export function Combobox({disabled, searchPlaceholder, selectPlaceholder, emptyP
   const [open, setOpen] = React.useState(false)
 
   return (
-    <Popover  open={open} onOpenChange={setOpen}>
+    <Popover  modal={true} open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           variant="outline"
